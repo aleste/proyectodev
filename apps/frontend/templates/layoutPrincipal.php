8ca68a06d442b22aa035388371aac0fcc22871ad
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-15 18:04:59 +0000

    Configuracion Inicial sfGuard

---
+++ apps/frontend/templates/layoutPrincipal.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+  <head>
+    <?php include_http_metas() ?>
+    <?php include_metas() ?>
+    <?php include_title() ?>
+    <link rel="shortcut icon" href="/favicon.ico" />
+  </head>
+
+    <body class="layout-two-column-right">
+        <div id="top"></div>
+        <div id="container">
+            <div id="container-inner" class="pkg">
+                
+                <div id="pagebody">
+                        <div id="pagebody-inner" class="pkg">
+                            <div class='hnav'>
+                            NAV
+                                <?php //include('_nav.php') ?>
+                            </div>
+                            <div id="alpha">
+                                <div id="alpha-inner" class="pkg content">
+                                   <?php echo $sf_content ?>
+                               </div>
+                              </div>
+                               <div class="clear">&nbsp;</div>
+                        </div>
+
+                </div>
+               
+            <div id="footer">
+              Pie
+              <?php //include('_footer.php') ?>
+            </div>
+
+            </div>
+        </div>
+    </body>
+</html>
