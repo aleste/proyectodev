a4559e72a8498d0e4c0f11b7f3eb6caeb8bfdcf8
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-15 18:04:59 +0000

    Configuracion Inicial sfGuard

--- trunk/apps/frontend/lib/myUser.class.php
+++ trunk/apps/frontend/lib/myUser.class.php
@@ -2,4 +2,16 @@
 
 class myUser extends sfGuardSecurityUser
 {
+      protected $aProfile;
+
+  /**
+   * getUserId function
+   *
+   * @return integer
+   * @author Pablo Sánchez <pablo.sanchez@aranova.es>
+   **/
+  public function getUserId() {
+    return parent::getGuardUser()->getId();
+  }
+
 }
