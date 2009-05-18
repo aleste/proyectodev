32dd6a238bdc9fc29dee6265fb6f20fbc407879d
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-18 18:55:14 +0000

    Configuracion Inicial sfGuard

---
+++ trunk/apps/frontend/templates/_footer.php
@@ -0,0 +1,4 @@
+<div id="footer-inner">
+  <?php echo mail_to('sistemas@pepe.gov', 'Departamento de Sistemas', 'title="' . __('Departamento de Sistemas') . '"').' | ' ?>
+  Usuario Activo: <?php // echo $sf_user->getId() ?>
+</div>
--- trunk/apps/frontend/templates/layoutLogin.php
+++ trunk/apps/frontend/templates/layoutLogin.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
   <head>
     <?php include_http_metas() ?>
@@ -11,36 +10,38 @@
         <div id="top"></div>
         <div id="container">
             <div id="container-inner" class="pkg">
-                
+            <div id="banner">
+                <div id="banner-inner" class="pkg">
+                    <h1 id="banner-header"></h1>
+                    <h2 id="banner-subject"></h2>
+                    <h3 id="banner-description"><?php echo __('Sistema de Gestión') ?></h3>
+                </div>
+            </div>
                 <div id="pagebody">
                         <div id="pagebody-inner" class="pkg">
-                      
                             <div id='hnav'>
-                                Sistema de Gestión
+                            
                             </div>
-
-                           
                              <div id='hnavmenu'>
                             <div id='navmenu'>
-                                    <?php include_partial('sfBreadNav/navmenu', array('menu' => 'menuPrincipal')) ?>
+                                    <?php //include_partial('sfBreadNav/navmenu', array('menu' => 'menuPrincipal')) ?>
                             </div>
 
                             </div>
-                            
+
                             <div id="alpha">
                                 <div id="alpha-inner" class="pkg content">
                                    <?php echo $sf_content ?>
                                </div>
                               </div>
-                              
                                <div class="clear">&nbsp;</div>
                         </div>
 
                 </div>
-               
+
             <div id="footer">
-              
-              <?php include('_footer.php') ?>
+              Pie
+              <?php //include('_footer.php') ?>
             </div>
 
             </div>
