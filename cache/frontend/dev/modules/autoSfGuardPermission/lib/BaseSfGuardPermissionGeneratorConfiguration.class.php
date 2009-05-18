617a02a15d5d699f1269b7fecbbbf675557a3ac0
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-18 03:15:39 +0000

    Configuracion del Menú de opciones y la administracion de usuarios

---
+++ cache/frontend/dev/modules/autoSfGuardPermission/lib/BaseSfGuardPermissionGeneratorConfiguration.class.php
@@ -0,0 +1,276 @@
+<?php
+
+/**
+ * sfGuardPermission module configuration.
+ *
+ * @package    ##PROJECT_NAME##
+ * @subpackage sfGuardPermission
+ * @author     ##AUTHOR_NAME##
+ * @version    SVN: $Id: configuration.php 17586 2009-04-24 18:25:44Z Kris.Wallsmith $
+ */
+class BaseSfGuardPermissionGeneratorConfiguration extends sfModelGeneratorConfiguration
+{
+  public function getCredentials($action)
+  {
+    if (0 === strpos($action, '_'))
+    {
+      $action = substr($action, 1);
+    }
+
+    return isset($this->configuration['credentials'][$action]) ? $this->configuration['credentials'][$action] : array();
+  }
+
+  public function getActionsDefault()
+  {
+    return array();
+  }
+
+  public function getFormActions()
+  {
+    return array(  '_delete' => NULL,  '_list' => NULL,  '_save' => NULL,  '_save_and_add' => NULL,);
+  }
+
+  public function getNewActions()
+  {
+    return array();
+  }
+
+  public function getEditActions()
+  {
+    return array();
+  }
+
+  public function getListObjectActions()
+  {
+    return array(  '_edit' => NULL,  '_delete' => NULL,);
+  }
+
+  public function getListActions()
+  {
+    return array(  '_new' => NULL,);
+  }
+
+  public function getListBatchActions()
+  {
+    return array(  '_delete' => NULL,);
+  }
+
+  public function getListParams()
+  {
+    return '%%id%% - %%name%% - %%description%%';
+  }
+
+  public function getListLayout()
+  {
+    return 'tabular';
+  }
+
+  public function getListTitle()
+  {
+    return 'Permission list';
+  }
+
+  public function getEditTitle()
+  {
+    return 'Editing Permission "%%name%%"';
+  }
+
+  public function getNewTitle()
+  {
+    return 'New Permission';
+  }
+
+  public function getFilterDisplay()
+  {
+    return array();
+  }
+
+  public function getFormDisplay()
+  {
+    return array();
+  }
+
+  public function getEditDisplay()
+  {
+    return array();
+  }
+
+  public function getNewDisplay()
+  {
+    return array();
+  }
+
+  public function getListDisplay()
+  {
+    return array(  0 => 'id',  1 => 'name',  2 => 'description',);
+  }
+
+  public function getFieldsDefault()
+  {
+    return array(
+      'id' => array(  'is_link' => true,  'is_real' => true,  'is_partial' => false,  'is_component' => false,  'type' => 'Text',),
+      'name' => array(  'is_link' => false,  'is_real' => true,  'is_partial' => false,  'is_component' => false,  'type' => 'Text',),
+      'description' => array(  'is_link' => false,  'is_real' => true,  'is_partial' => false,  'is_component' => false,  'type' => 'Text',),
+      'sf_guard_group_permission_list' => array(  'is_link' => false,  'is_real' => false,  'is_partial' => false,  'is_component' => false,  'type' => 'Text',),
+      'sf_guard_user_permission_list' => array(  'is_link' => false,  'is_real' => false,  'is_partial' => false,  'is_component' => false,  'type' => 'Text',),
+    );
+  }
+
+  public function getFieldsList()
+  {
+    return array(
+      'id' => array(),
+      'name' => array(),
+      'description' => array(),
+      'sf_guard_group_permission_list' => array(),
+      'sf_guard_user_permission_list' => array(),
+    );
+  }
+
+  public function getFieldsFilter()
+  {
+    return array(
+      'id' => array(),
+      'name' => array(),
+      'description' => array(),
+      'sf_guard_group_permission_list' => array(),
+      'sf_guard_user_permission_list' => array(),
+    );
+  }
+
+  public function getFieldsForm()
+  {
+    return array(
+      'id' => array(),
+      'name' => array(),
+      'description' => array(),
+      'sf_guard_group_permission_list' => array(),
+      'sf_guard_user_permission_list' => array(),
+    );
+  }
+
+  public function getFieldsEdit()
+  {
+    return array(
+      'id' => array(),
+      'name' => array(),
+      'description' => array(),
+      'sf_guard_group_permission_list' => array(),
+      'sf_guard_user_permission_list' => array(),
+    );
+  }
+
+  public function getFieldsNew()
+  {
+    return array(
+      'id' => array(),
+      'name' => array(),
+      'description' => array(),
+      'sf_guard_group_permission_list' => array(),
+      'sf_guard_user_permission_list' => array(),
+    );
+  }
+
+
+  /**
+   * Gets a new form object.
+   *
+   * @param  mixed $object
+   *
+   * @return sfForm
+   */
+  public function getForm($object = null)
+  {
+    $class = $this->getFormClass();
+
+    $form = new $class($object, $this->getFormOptions());
+
+    $this->fixFormFields($form);
+
+    return $form;
+  }
+
+  /**
+   * Gets the form class name.
+   *
+   * @return string The form class name
+   */
+  public function getFormClass()
+  {
+    return 'sfGuardPermissionForm';
+  }
+
+  public function getFormOptions()
+  {
+    return array();
+  }
+
+  public function hasFilterForm()
+  {
+    return true;
+  }
+
+  /**
+   * Gets the filter form class name
+   *
+   * @return string The filter form class name associated with this generator
+   */
+  public function getFilterFormClass()
+  {
+    return 'sfGuardPermissionFormFilter';
+  }
+
+  public function getFilterForm($filters)
+  {
+    $class = $this->getFilterFormClass();
+
+    return new $class($filters, $this->getFilterFormOptions());
+  }
+
+  public function getFilterFormOptions()
+  {
+    return array();
+  }
+
+  public function getFilterDefaults()
+  {
+    return array();
+  }
+
+  public function getPager($model)
+  {
+    $class = $this->getPagerClass();
+
+    return new $class($model, $this->getPagerMaxPerPage());
+  }
+
+  public function getPagerClass()
+  {
+    return 'sfPropelPager';
+  }
+
+  public function getPagerMaxPerPage()
+  {
+    return 20;
+  }
+
+  public function getDefaultSort()
+  {
+    return array(null, null);
+  }
+
+  public function getPeerMethod()
+  {
+    return 'doSelect';
+  }
+
+  public function getPeerCountMethod()
+  {
+    return 'doCount';
+  }
+
+  public function getConnection()
+  {
+    return null;
+  }
+}
