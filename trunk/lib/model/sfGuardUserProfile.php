676e5d12adb492dd47778eb26e8ea705ae83f93f
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-20 18:36:30 +0000

    Integrando sfGuard con Datos Usuario

--- trunk/lib/model/sfGuardUserProfile.php
+++ trunk/lib/model/sfGuardUserProfile.php
@@ -4,13 +4,19 @@ class sfGuardUserProfile extends BasesfGuardUserProfile
 {
      protected $user = null;
 
-  public function __toString() {
+/*  public function __toString() {
     return $this->getApellido();
-  }
+  }*/
    public function getGuardUser() {
     if (!$this->user) {
       $this->user = sfGuardUserPeer::retrieveByPk($this->getUserId());
     }
     return $this->user;
   }
+    public function getNombreCompleto() {
+        $nombre = $this->getGuardUser()->getProfile()->getNombre();
+        $apellido = $this->getGuardUser()->getProfile()->getApellido();
+        return $nombre. ', '. $apellido;
+    }
+
 }
