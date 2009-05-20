7cc28adb3d612627faa21bccc39cebeb39b4b02c
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-20 03:52:29 +0000

    Extensión sfGuard con sfUserProfile. Se creo modulo Usuario

---
+++ trunk/data/sql/lib.model.schema.sql
@@ -0,0 +1,28 @@
+
+# This is a fix for InnoDB in MySQL >= 4.1.x
+# It "suspends judgement" for fkey relationships until are tables are set.
+SET FOREIGN_KEY_CHECKS = 0;
+
+#-----------------------------------------------------------------------------
+#-- sf_guard_user_profile
+#-----------------------------------------------------------------------------
+
+DROP TABLE IF EXISTS `sf_guard_user_profile`;
+
+
+CREATE TABLE `sf_guard_user_profile`
+(
+	`id` INTEGER  NOT NULL AUTO_INCREMENT,
+	`user_id` INTEGER  NOT NULL,
+	`apellido` VARCHAR(50),
+	`nombre` VARCHAR(50),
+	PRIMARY KEY (`id`),
+	INDEX `sf_guard_user_profile_FI_1` (`user_id`),
+	CONSTRAINT `sf_guard_user_profile_FK_1`
+		FOREIGN KEY (`user_id`)
+		REFERENCES `sf_guard_user` (`id`)
+		ON DELETE CASCADE
+)Type=InnoDB;
+
+# This restores the fkey checks, after having unset them earlier
+SET FOREIGN_KEY_CHECKS = 1;
