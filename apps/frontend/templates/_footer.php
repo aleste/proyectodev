0471827db6b32156980c37d3645b1be9715013a0
asteinmetz78 <asteinmetz78@ab45aa52-3f3e-11de-bd56-27b9897bf3d5>
2009-05-20 18:36:42 +0000

    Integrando sfGuard con Datos Usuario

--- apps/frontend/templates/_footer.php
+++ apps/frontend/templates/_footer.php
@@ -1,5 +1,6 @@
 <div id="footer-inner">
   <?php echo mail_to('sistemas@pepe.gov', 'Departamento de Sistemas', 'title="' . __('Departamento de Sistemas') . '"').' | ' ?>
-  Usuario Activo: <b> <?php  echo $sf_user->getGuardUser()->getUsername() ?></b>
+  Usuario Activo: <b> <?php  echo $sf_user->getProfile()->getNombreCompleto() ?></b>
 
 </div>
+
